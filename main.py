350ac278525b17758be85bd12a86f15acee8e320
pc3541 <96953899+pc3541@users.noreply.github.com>
2022-08-02 03:48:50 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#Streamlit
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from soustitle import Subtitle
+import io
+
+st.sidebar.title("Subtitle Cleaning")
+input_srt = st.sidebar.file_uploader("Upload SRT file:")
+input_scc = st.sidebar.file_uploader("Upload SCC file:")
+
+
+def run():
+    srt = Subtitle(input_srt)
+    srt_file = srt.open()
+    df = pd.read_csv(srt_file, delim_whitespace=True, header=None)
+    print(df)
+
+if st.sidebar.button("Run cleaning"):
+    run()
