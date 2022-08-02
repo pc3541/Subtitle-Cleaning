6c2ad09468eda18817b45d49784335b715eeba68
pc3541 <96953899+pc3541@users.noreply.github.com>
2022-08-02 13:04:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def run():
     data2 = stringio2.read()
     pysubs = SCCReader().read(data2)
     data3 = pysubs.get_captions(lang="en-US")
-    st.write(data3
+    st.write(data3)
     
 if st.sidebar.button("Run cleaning"):
     run()
