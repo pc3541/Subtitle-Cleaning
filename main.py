7d3681afa01f732ad47d65606e953a5dd85a97ce
pc3541 <96953899+pc3541@users.noreply.github.com>
2022-08-02 18:15:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -21,45 +21,47 @@ def run():
     df_srt = pd.DataFrame()
     df_scc = pd.DataFrame()
     
-    stringio = io.StringIO(input_srt.getvalue().decode("utf-8"))
-    data = stringio.read()
-    subtitle_generator = srt.parse(data)
-    subtitles = list(subtitle_generator)
-    for x in range(len(subtitles)):
-        subtitles[x].start = srt.timedelta_to_srt_timestamp(subtitles[x].start)
-        starts_srt.append(subtitles[x].start)
-        subtitles[x].end = srt.timedelta_to_srt_timestamp(subtitles[x].end)
-        ends_srt.append(subtitles[x].end)
-        subs_srt.append(subtitles[x].content)
-    df_srt["Starts"] = starts_srt
-    df_srt["Ends"] = ends_srt
-    df_srt["Subtitles"] = subs_srt
-    st.write("Uploaded subtitles (SRT):")
-    st.dataframe(df_srt)
-    st.write("")
-    st.write("Filtered subtitles (SRT, keyword: 'you'):")
-    filtered = df_srt[df_srt['Subtitles'].str.contains("you")]
-    st.dataframe(filtered) 
-
-    pysubs = SCCReader().read(input_scc.getvalue().decode("utf-8"))
-    final = SRTWriter().write(pysubs)
-    subtitle_generator2 = srt.parse(final)
-    subtitles2 = list(subtitle_generator2)
-    for x in range(len(subtitles2)):
-        subtitles2[x].start = srt.timedelta_to_srt_timestamp(subtitles2[x].start)
-        starts_scc.append(subtitles2[x].start)
-        subtitles2[x].end = srt.timedelta_to_srt_timestamp(subtitles2[x].end)
-        ends_scc.append(subtitles2[x].end)
-        subs_scc.append(subtitles2[x].content)
-    df_scc["Starts"] = starts_scc
-    df_scc["Ends"] = ends_scc
-    df_scc["Subtitles"] = subs_scc
-    st.write("Uploaded subtitles (SCC):")
-    st.dataframe(df_scc)
-    st.write("")
-    st.write("Filtered subtitles (SCC, keyword: 'me'):")
-    filtered_scc = df_scc[df_scc['Subtitles'].str.contains("me")]
-    st.dataframe(filtered_scc) 
+    if input_srt is not None:
+        stringio = io.StringIO(input_srt.getvalue().decode("utf-8"))
+        data = stringio.read()
+        subtitle_generator = srt.parse(data)
+        subtitles = list(subtitle_generator)
+        for x in range(len(subtitles)):
+            subtitles[x].start = srt.timedelta_to_srt_timestamp(subtitles[x].start)
+            starts_srt.append(subtitles[x].start)
+            subtitles[x].end = srt.timedelta_to_srt_timestamp(subtitles[x].end)
+            ends_srt.append(subtitles[x].end)
+            subs_srt.append(subtitles[x].content)
+        df_srt["Starts"] = starts_srt
+        df_srt["Ends"] = ends_srt
+        df_srt["Subtitles"] = subs_srt
+        st.write("Uploaded subtitles (SRT):")
+        st.dataframe(df_srt)
+        st.write("")
+        st.write("Filtered subtitles (SRT, keyword: 'you'):")
+        filtered = df_srt[df_srt['Subtitles'].str.contains("you")]
+        st.dataframe(filtered) 
+    
+    if input_scc is not None:
+        pysubs = SCCReader().read(input_scc.getvalue().decode("utf-8"))
+        final = SRTWriter().write(pysubs)
+        subtitle_generator2 = srt.parse(final)
+        subtitles2 = list(subtitle_generator2)
+        for x in range(len(subtitles2)):
+            subtitles2[x].start = srt.timedelta_to_srt_timestamp(subtitles2[x].start)
+            starts_scc.append(subtitles2[x].start)
+            subtitles2[x].end = srt.timedelta_to_srt_timestamp(subtitles2[x].end)
+            ends_scc.append(subtitles2[x].end)
+            subs_scc.append(subtitles2[x].content)
+        df_scc["Starts"] = starts_scc
+        df_scc["Ends"] = ends_scc
+        df_scc["Subtitles"] = subs_scc
+        st.write("Uploaded subtitles (SCC):")
+        st.dataframe(df_scc)
+        st.write("")
+        st.write("Filtered subtitles (SCC, keyword: 'me'):")
+        filtered_scc = df_scc[df_scc['Subtitles'].str.contains("me")]
+        st.dataframe(filtered_scc) 
     
 if st.sidebar.button("Run cleaning"):
     run()
