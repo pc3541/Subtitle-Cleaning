fc43ad6c533ed0d679f01aa4bb478bbf7422a1dc
pc3541 <96953899+pc3541@users.noreply.github.com>
2022-08-02 13:06:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,11 +12,7 @@ input_srt = st.sidebar.file_uploader("Upload SRT file:", type=['srt'])
 input_scc = st.sidebar.file_uploader("Upload SCC file:", type=['scc'])
 
 def run():
-
-    
-    stringio2 = io.StringIO(input_scc.getvalue().decode("utf-8"))
-    data2 = stringio2.read()
-    pysubs = SCCReader().read(data2)
+    pysubs = SCCReader().read(input_scc.getvalue().decode("utf-8"))
     data3 = pysubs.get_captions(lang="en-US")
     st.write(data3)
     
