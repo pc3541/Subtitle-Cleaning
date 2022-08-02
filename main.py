aac022b569c15f0d624b0087338085b5155ae561
pc3541 <96953899+pc3541@users.noreply.github.com>
2022-08-02 04:44:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,20 +12,8 @@ input_scc = st.sidebar.file_uploader("Upload SCC file:")
 
 
 def run():
-    starts = []
-    ends = []
-    subtitles = []
-    df = pd.DataFrame()
-    content = input_srt.getvalue()
-    subs = pysrt.open(content)
-    for sub in subs:
-        starts.append(sub.start)
-        ends.append(sub.end)
-        subtitles.append(sub.text)
-    df["Starts"] = starts
-    df["Ends"] = ends
-    df["Subtitles"] = subtitles
-    print(df)
+    dataframe = pd.read_csv(inpurt_srt)
+    st.write(dataframe)
 
 if st.sidebar.button("Run cleaning"):
     run()
